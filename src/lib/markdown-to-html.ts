2eddd14259949cae99a45c7defce2684efeec2bc
Kori Ellis <koriellis@Koris-CumPewTour.local>
2025-12-14 22:50:53 -0600

    Fix markdown-to-html: remove deprecated headerIds option from marked configuration

--- src/lib/markdown-to-html.ts
+++ src/lib/markdown-to-html.ts
@@ -40,7 +40,6 @@ export function markdownToHTML(markdown: string | null | undefined): string {
   marked.setOptions({
     gfm: true, // GitHub Flavored Markdown
     breaks: false, // Don't convert line breaks to <br>
-    headerIds: false, // Don't add IDs to headers
     mangle: false, // Don't mangle email addresses
   })
 
